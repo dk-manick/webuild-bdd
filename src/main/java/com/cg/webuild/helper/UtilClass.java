4394a175974d993a57162c6811b5771a1a112ad3
Dakshinamoorthy Manickavasagam <manickavasagam.dakshinamoorthy@capgemini.com>
2020-10-07 11:44:50 +0800

    1) Added TestNG, Junit and Extent reports dependencies in pom.xml file 2) Added required reusable methods in util class file

--- src/main/java/com/cg/webuild/helper/UtilClass.java
+++ src/main/java/com/cg/webuild/helper/UtilClass.java
@@ -1,9 +1,19 @@
 package com.cg.webuild.helper;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
+
+import org.apache.commons.io.FileUtils;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -25,4 +35,38 @@ public class UtilClass {
 		wait.until(ExpectedConditions.elementToBeClickable(bylocator));
 	    driver.findElement(bylocator).click();
 	}
+	
+	public void mouseoverElementAndClick(WebDriver driver, WebElement element, By locator) {
+		wait = new WebDriverWait(driver, 60);
+		wait.until(ExpectedConditions.visibilityOf(element));
+		wait.until(ExpectedConditions.elementToBeClickable(element));
+		Actions a = new Actions(driver);
+		a.moveToElement(element).build().perform();
+		wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+		a.moveToElement(driver.findElement(locator)).click().build().perform();
+	}
+
+	public void setText(WebDriver driver, WebElement element, String text) {
+		wait = new WebDriverWait(driver, 60);
+		wait.until(ExpectedConditions.visibilityOf(element));
+		wait.until(ExpectedConditions.elementToBeClickable(element));
+		element.clear();
+		element.sendKeys(text);
+	}
+	
+	public String getText(WebDriver driver, WebElement element) {
+		wait = new WebDriverWait(driver, 60);
+		wait.until(ExpectedConditions.visibilityOf(element));
+		Log.info("Element text : "+ element.getText());
+		return element.getText();
+	}
+	
+	public String getText(WebDriver driver, By locator) {
+		wait = new WebDriverWait(driver, 60);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+		String value = driver.findElement(locator).getText();
+		Log.info("Element text : "+ value);
+		return value;
+	}
+
 }
